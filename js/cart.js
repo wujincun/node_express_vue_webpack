0562e86bbe8d6bd66714fa32857a3eb238da1d41
wujincun <1769453779@qq.com>
2017-03-26 17:24:19 +0800

    引入 vue，简单的demo

---
+++ js/cart.js
@@ -0,0 +1,17 @@
+
+/**
+ * Created by Administrator on 2017/3/26.
+ */
+new Vue({
+    el:'#app',
+    data(){
+        return{
+            message:null
+        }
+    },
+    methods:{
+        clickMe(){
+            alert(1)
+        }
+    }
+})
